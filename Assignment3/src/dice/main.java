d70c9d1cbb8733d131fcc1e5cbe952c50152ef67
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-03-28 14:21:37 -0500

    simple two die test run added to main

--- Assignment3/src/dice/main.java
+++ Assignment3/src/dice/main.java
@@ -5,6 +5,24 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("hello World!");
+		die die1 = new die();
+		die die2 = new die();
+		
+		int die1Tot =0;
+		int die2Tot = 0;
+		int total = 0;
+		
+		for (int i = 1; i<4; i++) {
+			 die1.roll();
+			int temp1 = die1.getFaceValue();
+			die2.roll();
+			int temp2 = die2.getFaceValue();
+			int sub = temp1 + temp2;
+			total = total +sub;
+			System.out.printf("\nRole %d: generated a %d and a %d for a total of %d", i, temp1,temp2, sub);
+			
+		}
+		System.out.println("\nTotal of three tosses is: " + total);
 	}
 
 }
