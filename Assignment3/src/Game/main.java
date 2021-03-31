98855180b41de634c812066dac6e3be36d28bc3b
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-03-31 07:21:55 -0500

    Added a roll test and corrected a change to roll method to correct an outside of appropriate range error

--- Assignment3/src/Game/main.java
+++ Assignment3/src/Game/main.java
@@ -4,26 +4,13 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Die die1 = new Die();
-		Die die2 = new Die();
 		
-		int total = 0;
-		
-		for (int i = 1; i<4; i++) {
-			 die1.roll();
-			int temp1 = die1.getFaceValue();
-			die2.roll();
-			int temp2 = die2.getFaceValue();
-			int sub = temp1 + temp2;
-			total = total +sub;
-			System.out.printf("\nRole %d: generated a %d and a %d for a total of %d", i, temp1,temp2, sub);
-			
-		}
-		System.out.println("\nTotal of three tosses is: " + total);
 		
 		test tester = new test();
+		tester.gameTest();
 		tester.dieTest();
 		tester.profTest();
+		tester.sideTest();
 	}
 
 }
--- Assignment3/src/Game/test.java
+++ Assignment3/src/Game/test.java
@@ -13,8 +13,25 @@ public class test {
     	System.out.println("test die 3 has "+test3.getNumFaces()+  " sides!\n and a face value of "
     			+ test3.getFaceValue());
     	System.out.println("test4: " +test4);
-    	
-    	
+    }
+    
+    public void gameTest() {
+    	Die die1 = new Die();
+		Die die2 = new Die();
+		
+		int total = 0;
+		
+		for (int i = 1; i<4; i++) {
+			 die1.roll();
+			int temp1 = die1.getFaceValue();
+			die2.roll();
+			int temp2 = die2.getFaceValue();
+			int sub = temp1 + temp2;
+			total = total +sub;
+			System.out.printf("\nRole %d: generated a %d and a %d for a total of %d", i, temp1,temp2, sub);
+			
+		}
+		System.out.println("\nTotal of three tosses is: " + total);
     }
     
     public void profTest() {
@@ -46,5 +63,26 @@ public class test {
     	System.out.println("rolling d1");
     	d1.roll();
     	System.out.println("d1: " + d1);
+    }
+    
+    public void sideTest(){
+    	Die die1, die2;
+    	die1 =new Die(8,8);
+    	//die2 = new Die(4,6);
+    	boolean check = true;
+    	
+    	for (int i= 1; i <= 1000; i++) {
+    		die1.roll();
+    	//	die2.roll();
+    		if (die1.getFaceValue() == 0 || die1.getFaceValue() > die1.getNumFaces()) {
+    			check = false;
+    			System.out.println("The Die Rolled an invalid number: " + die1.getFaceValue() +
+    			" which is outside the range of 1 to " + die1.getNumFaces() + " this failure ocurred on roll " + i +".");
+    			break;
+    		} 
+    		
+    	}
+    	if (check) { System.out.println("The roll test passed!");
+    	}
     }
 }
